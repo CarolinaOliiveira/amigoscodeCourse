3edc204e939e1b9179d12d213414b2de3ab1610c
CarolinaOliiveira <carolina.c.oliveira@gmail.com>
2024-01-26 17:04:57 +0000

    Create customer frontend working with backend

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -3,19 +3,33 @@ import SidebarWithHeader from './components/shared/SideBar.jsx'
 import  { useEffect, useState } from 'react'
 import { getCustomers } from './services/client.js'
 import CardWithImage from './components/Card.jsx'
+import DrawerForm from './components/DrawerForm.jsx'
+import {errorNotification } from './services/notification.js'
+
 function App() {
 
     const [customers, setCustomers] = useState([])
     const [loading, setLoading] = useState(false)
-    useEffect(() => {
+    const [err, setErr] = useState("")
+
+
+    const fetchCustomers = () => {
         setLoading(true)
         getCustomers().then(res => {
             setCustomers(res.data)
         }).catch(err =>{
-            console.log(err)
+            setErr(err.response.data.message)
+            errorNotification(
+                err.code,
+                err.response.data.message
+            )
         }).finally(() => {
             setLoading(false)
         })
+    }
+
+    useEffect(() => {
+        fetchCustomers()
     }, [])
 
     if(loading) {
@@ -32,10 +46,24 @@ function App() {
         )
     }
 
+    if(err){
+        return (
+            <SidebarWithHeader>
+                <DrawerForm
+                    fetchCustomers={fetchCustomers}
+                />
+                <Text mt={5}> Ooops there was an error</Text>
+            </SidebarWithHeader>
+        )
+    }
+
     if(customers.length <= 0) {
         return (
             <SidebarWithHeader>
-                <Text>No customers available</Text>
+                <DrawerForm
+                    fetchCustomers={fetchCustomers}
+                />
+                <Text mt={5}>No customers available</Text>
             </SidebarWithHeader>
         )
     }
@@ -43,6 +71,9 @@ function App() {
     //opção em que temos customers
     return (
         <SidebarWithHeader>
+            <DrawerForm
+                fetchCustomers={fetchCustomers}
+            />
             <Wrap justify={"center"} spacing ={'30px'}>
                 {customers.map((customer, index) => {
                     return (
