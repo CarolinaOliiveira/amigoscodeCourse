e066becf76f0e6ab1a52ef98d7e03fbf3c5e80cb
CarolinaOliiveira <carolina.c.oliveira@gmail.com>
2024-02-05 14:26:37 +0000

    React login e registo

--- frontend/react/src/components/CreateCustomerDrawer.jsx
+++ frontend/react/src/components/CreateCustomerDrawer.jsx
@@ -10,7 +10,7 @@ import {
     DrawerCloseButton,
     useDisclosure
 } from '@chakra-ui/react'
-import CreateCustomerForm from './CreateCustomerForm.jsx'
+import CreateCustomerForm from './shared/CreateCustomerForm.jsx'
 
 const AddIcon = () =>  "+"
 const CloseIcon = () =>  "x"
@@ -31,7 +31,7 @@ const CreateCustomerDrawer = ({fetchCustomers}) => {
 
                 <DrawerBody>
                     <CreateCustomerForm
-                        fetchCustomers={ fetchCustomers }
+                        onSuccess={ fetchCustomers }
                     />
                 </DrawerBody>
 
